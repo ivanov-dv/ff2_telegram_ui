be62a7dec1f9104071fecea99955899ba67fd235
Denis Ivanov <dnb8866@gmail.com>
2024-12-17 18:25:51 +0700

    Добавлены docstrings

--- src/handlers/registration_handlers.py
+++ src/handlers/registration_handlers.py
@@ -10,6 +10,7 @@ router.message.filter(F.chat.type.in_('private'))
 
 @router.callback_query(F.data == 'registration_delete')
 async def registration_delete(callback: types.CallbackQuery):
+    """Предупреждающее сообщение перед удалением аккаунта."""
     await callback.message.edit_text(
         setting_texts.DELETE_USER,
         reply_markup=keyboards.RegistrationKb.confirm_delete_registration()
@@ -18,6 +19,7 @@ async def registration_delete(callback: types.CallbackQuery):
 
 @router.callback_query(F.data == 'registration_delete_accept')
 async def registration_delete_accept(callback: types.CallbackQuery):
+    """Удаление аккаунта."""
     await backend_client.delete_user(callback.from_user.id)
     await callback.message.edit_text(
         setting_texts.SUCCESS_DELETE_USER,
@@ -27,6 +29,7 @@ async def registration_delete_accept(callback: types.CallbackQuery):
 
 @router.callback_query(F.data == 'registration_delete_cancel')
 async def registration_delete_cancel(callback: types.CallbackQuery):
+    """Отмена удаления аккаунта."""
     await callback.message.edit_text(
         setting_texts.CANCEL_DELETE_USER,
         reply_markup=keyboards.FamilyFinanceKb.go_to_main()
