df9f73659da705eaff317fc73dd81e5a652de26a
Denis Ivanov <ivanov.denis@bk.ru>
2025-04-20 13:51:31 +0700

    Изменен порядок кнопок для выбора периода.

--- src/utils/keyboard_generators.py
+++ src/utils/keyboard_generators.py
@@ -30,16 +30,16 @@ class GeneratorKb:
                 callback_data=f'period_12_{today.year - 1}'
             )
             builder.button(
+                text=f'01_{today.year}',
+                callback_data=f'period_01_{today.year}'
+            )
+            builder.button(
                 text=f'02_{today.year}',
                 callback_data=f'period_02_{today.year}'
             )
             builder.button(
                 text=f'03_{today.year}',
                 callback_data=f'period_03_{today.year}'
-            )
-            builder.button(
-                text=f'01_{today.year}',
-                callback_data=f'period_01_{today.year}'
             )
 
         # Если текущий месяц февраль.
@@ -53,16 +53,16 @@ class GeneratorKb:
                 callback_data=f'period_01_{today.year}'
             )
             builder.button(
+                text=f'02_{today.year}',
+                callback_data=f'period_02_{today.year}'
+            )
+            builder.button(
                 text=f'03_{today.year}',
                 callback_data=f'period_03_{today.year}'
             )
             builder.button(
                 text=f'04_{today.year}',
                 callback_data=f'period_04_{today.year}'
-            )
-            builder.button(
-                text=f'02_{today.year}',
-                callback_data=f'period_02_{today.year}'
             )
 
         # Если текущий месяц ноябрь.
@@ -76,16 +76,16 @@ class GeneratorKb:
                 callback_data=f'period_10_{today.year}'
             )
             builder.button(
+                text=f'11_{today.year}',
+                callback_data=f'period_11_{today.year}'
+            )
+            builder.button(
                 text=f'12_{today.year}',
                 callback_data=f'period_12_{today.year}'
             )
             builder.button(
                 text=f'01_{today.year}',
                 callback_data=f'period_01_{today.year + 1}'
-            )
-            builder.button(
-                text=f'11_{today.year}',
-                callback_data=f'period_11_{today.year}'
             )
 
         # Если текущий месяц декабрь.
@@ -99,6 +99,10 @@ class GeneratorKb:
                 callback_data=f'period_11_{today.year}'
             )
             builder.button(
+                text=f'12_{today.year}',
+                callback_data=f'period_12_{today.year}'
+            )
+            builder.button(
                 text=f'01_{today.year + 1}',
                 callback_data=f'period_01_{today.year + 1}'
             )
@@ -106,15 +110,10 @@ class GeneratorKb:
                 text=f'02_{today.year + 1}',
                 callback_data=f'period_02_{today.year + 1}'
             )
-            builder.button(
-                text=f'12_{today.year}',
-                callback_data=f'period_12_{today.year}'
-            )
 
         # В остальных случаях.
         else:
-            offset_list = [-2, -1, 1, 2]
-            for offset in offset_list:
+            for offset in range(-2, 3):
                 builder.button(
                     text=f'{str(today.month + offset).zfill(2)}_'
                          f'{today.year}',
@@ -122,12 +121,7 @@ class GeneratorKb:
                                   f'{str(today.month + offset).zfill(2)}_'
                                   f'{today.year}'
                 )
-            builder.button(
-                text=f'{str(today.month).zfill(2)}_{today.year}',
-                callback_data=f'period_{str(today.month).zfill(2)}_'
-                              f'{today.year}'
-            )
-        return builder.adjust(2)
+        return builder.adjust(2, 1, 2)
 
     @staticmethod
     def generate_choose_space(
